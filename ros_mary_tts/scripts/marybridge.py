524c068421fa6c50f66e8feebe37f449f98d57c8
Marc Hanheide <marc@hanheide.net>
2013-07-23 14:05:31 +0100

    added as a an actionlib server

--- ros_mary_tts/scripts/marybridge.py
+++ ros_mary_tts/scripts/marybridge.py
@@ -15,6 +15,26 @@ import logging
 from ros_mary_tts.srv import *
 import rospy
 
+import actionlib
+import ros_mary_tts.msg
+
+
+
+
+
+
+
+speakQueue = Queue()
+replyQueue=Queue()
+
+def speak(req):
+
+    speakQueue.put(req.text)
+    return True
+
+def speak_server():
+    s = rospy.Service('ros_mary', ros_mary, speak)
+    print "Ready to speak."
 
 
 
@@ -36,6 +56,9 @@ class maryclient:
         self.audio = "WAVE_FILE"
         self.locale = "en_US"
         self.voice = "cmu-bdl-hsmm"
+        self._action_name = "speak"
+        self._as = actionlib.SimpleActionServer(self._action_name, ros_mary_tts.msg.maryttsAction, execute_cb=self.execute_cb)
+        self._as.start()
 
     def set_host(self, a_host):
         """Set the host for the TTS server."""
@@ -137,6 +160,14 @@ class maryclient:
                 response.reason))
         return response.read()
 
+    def execute_cb(self, goal):
+        # helper variables
+        rospy.loginfo("action triggered: say %s", goal.text);
+        speakQueue.put(goal.text)
+        replyQueue.get(True, 10);
+        rospy.loginfo("finished speaking...");
+        self._as.set_succeeded(True);
+
 
 class pulseplayer:
         def __init__(self, name):
@@ -144,97 +175,88 @@ class pulseplayer:
 
         def play(self, a_sound):
 
-                buf = StringIO.StringIO(a_sound)
-		#
-		# pulseaudio / playback stuff
-		#
-
-		pa = ctypes.cdll.LoadLibrary('libpulse-simple.so.0')
- 
-		PA_STREAM_PLAYBACK = 1
-		PA_SAMPLE_S16LE = 3
-		BUFFSIZE = 1024
-
-		class struct_pa_sample_spec(ctypes.Structure):
-		    __slots__ = [
-		        'format',
-        		'rate',
-		        'channels',
-		    ]
- 
-		struct_pa_sample_spec._fields_ = [
-		    ('format', ctypes.c_int),
-		    ('rate', ctypes.c_uint32),
-		    ('channels', ctypes.c_uint8),
-		]
-		pa_sample_spec = struct_pa_sample_spec  # /usr/include/pulse/sample.h:174
-                
-                wf = wave.open(buf, 'rb')
-                ss = struct_pa_sample_spec()
-                ss.rate = wf.getframerate()
-                ss.channels = wf.getnchannels()
-                ss.format = PA_SAMPLE_S16LE
-                error = ctypes.c_int(0)
-
-
-                s = pa.pa_simple_new(
-                    None,  # Default server.
-                    'marybridge',  # Application's name.
-                    PA_STREAM_PLAYBACK,  # Stream for playback.
-                    None,  # Default device.
-                    'playback',  # Stream's description.
-                    ctypes.byref(ss),  # Sample format.
-                    None,  # Default channel map.
-                    None,  # Default buffering attributes.
-                    ctypes.byref(error)  # Ignore error code.
-                )
-                if not s:
-                    raise Exception('Could not create pulse audio stream: {0}!'.format(
-                        pa.strerror(ctypes.byref(error))))
+            buf = StringIO.StringIO(a_sound)
+            #
+            # pulseaudio / playback stuff
+            #
     
-                while True:
-                    # latency = pa.pa_simple_get_latency(s, error)
-                    # if latency == -1:
-                    #    raise Exception('Getting latency failed!')
-                
-                    # print('{0} usec'.format(latency))
-                
-                    # Reading frames and writing to the stream.
-                    buf = wf.readframes(BUFFSIZE)
-                    if buf == '':
-                        break
-                
-                    if pa.pa_simple_write(s, buf, len(buf), error):
-                        raise Exception('Could not play file!')
-                
-                wf.close()
-                
-                # Waiting for all sent data to finish playing.
-                if pa.pa_simple_drain(s, error):
-                    raise Exception('Could not simple drain!')
-                
-                # Freeing resources and closing connection.
-                pa.pa_simple_free(s)
+            pa = ctypes.cdll.LoadLibrary('libpulse-simple.so.0')
+     
+            PA_STREAM_PLAYBACK = 1
+            PA_SAMPLE_S16LE = 3
+            BUFFSIZE = 1024
+    
+            class struct_pa_sample_spec(ctypes.Structure):
+                __slots__ = [
+                    'format',
+                    'rate',
+                    'channels',
+                ]
+     
+            struct_pa_sample_spec._fields_ = [
+                ('format', ctypes.c_int),
+                ('rate', ctypes.c_uint32),
+                ('channels', ctypes.c_uint8),
+            ]
+            pa_sample_spec = struct_pa_sample_spec  # /usr/include/pulse/sample.h:174
+                    
+            wf = wave.open(buf, 'rb')
+            ss = struct_pa_sample_spec()
+            ss.rate = wf.getframerate()
+            ss.channels = wf.getnchannels()
+            ss.format = PA_SAMPLE_S16LE
+            error = ctypes.c_int(0)
+    
+    
+            s = pa.pa_simple_new(
+                None,  # Default server.
+                'marybridge',  # Application's name.
+                PA_STREAM_PLAYBACK,  # Stream for playback.
+                None,  # Default device.
+                'playback',  # Stream's description.
+                ctypes.byref(ss),  # Sample format.
+                None,  # Default channel map.
+                None,  # Default buffering attributes.
+                ctypes.byref(error)  # Ignore error code.
+            )
+            if not s:
+                raise Exception('Could not create pulse audio stream: {0}!'.format(
+                    pa.strerror(ctypes.byref(error))))
+    
+            while True:
+                # latency = pa.pa_simple_get_latency(s, error)
+                # if latency == -1:
+                #    raise Exception('Getting latency failed!')
+            
+                # print('{0} usec'.format(latency))
+            
+                # Reading frames and writing to the stream.
+                buf = wf.readframes(BUFFSIZE)
+                if buf == '':
+                    break
+            
+                if pa.pa_simple_write(s, buf, len(buf), error):
+                    raise Exception('Could not play file!')
+            
+            wf.close()
+            
+            # Waiting for all sent data to finish playing.
+            if pa.pa_simple_drain(s, error):
+                raise Exception('Could not simple drain!')
+            
+            # Freeing resources and closing connection.
+            pa.pa_simple_free(s)
 
-client = maryclient()
+
+
 player = pulseplayer("Mary")
-
-q = Queue()
-
-def speak(req):
-
-    q.put(req)
-    return True
-
-def speak_server():
-    rospy.init_node('mary_tts')
-    s = rospy.Service('ros_mary', ros_mary, speak)
-    print "Ready to speak."
-
 
 
 
 if __name__ == "__main__":
+    rospy.init_node('mary_tts')
+
+    client = maryclient()
     speak_server()
     client.set_locale ("en_US")
     # client.set_locale ("de")
@@ -272,15 +294,15 @@ if __name__ == "__main__":
     # client.set_voice ("dfki-ot-hsmm")
 
     while not rospy.is_shutdown():
-	try:
-	    req = q.get(True, 1)
-	    rospy.loginfo("say " + req.text)
-    	    the_sound = client.generate(req.text)
-	    player.play(the_sound)
-	    rospy.loginfo("played")
-     	except Empty:
-	    rospy.logdebug('nothing sent')
-
+        try:
+            req = speakQueue.get(True, 1)
+            rospy.loginfo("say " + req)
+            the_sound = client.generate(req)
+            player.play(the_sound)
+            rospy.loginfo("played")
+            replyQueue.put(True)
+        except Empty:
+            rospy.logdebug('nothing sent')
 
 
 

0e28b1ed0121243c9c7dc815a8fb00508dff1927
peter Lightbody <pet1330@gmail.com>
2013-07-24 14:09:07 +0100

    This version works with Transform - tested

--- src/vars.cpp
+++ src/vars.cpp
@@ -16,23 +16,30 @@ vars::vars() : node("attention_head") {
 
 void vars::locationCallback(const cob_people_detection_msgs::DetectionArray::ConstPtr& detectionArray) {
     z = 15;
-
+    
+    bool found=false;
     geometry_msgs::PoseStamped poseInCamCoords;
     geometry_msgs::PoseStamped poseInRobotCoords;
 
     for (unsigned i = 0; i < detectionArray->detections.size(); i++) {
 
         if (detectionArray->detections[i].pose.pose.position.z < z) {
-            z = detectionArray->detections[i].pose.pose.position.z;
-            poseInCamCoords = detectionArray->detections[i].pose;
+z =detectionArray->detections[i].pose.pose.position.z;         
+   poseInCamCoords = detectionArray->detections[i].pose;
+poseInCamCoords.pose.orientation.w =1.0;
+poseInCamCoords.pose.position.x*=-1.0;
+poseInCamCoords.pose.position.y*=-1.0;
+            found=true;
         }
     }
-
-    poseInCamCoords.header.frame_id = "/head_xtion_depth_optical_frame";
+    if (!found)
+	return;
+    poseInCamCoords.header.frame_id = "/head_xtion_rgb_optical_frame";
     try {
         listener.transformPose("/head_base_frame", poseInCamCoords, poseInRobotCoords);
-    } catch (tf::TransformException& ex) {
+    } catch (tf::TransformException ex) {
         ROS_ERROR("%s", ex.what());
+
     }
 
     x = poseInRobotCoords.pose.position.x;
@@ -40,7 +47,7 @@ void vars::locationCallback(const cob_people_detection_msgs::DetectionArray::Con
     z = poseInRobotCoords.pose.position.z;
 
 
-    double threshold = 0.2;
+    double threshold = 0.1;
     if ((x - last_x > threshold) ||
             (x - last_x < -threshold) ||
             (y - last_y > threshold) ||

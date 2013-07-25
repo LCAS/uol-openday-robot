9ed5ab8ceae873f5cd7c6f9fba8c988b4ebd0af4
cdondrup <cdondrup@lincoln.ac.uk>
2013-07-25 15:20:15 +0100

    Just talk once.

--- uol_patroller/scripts/engage_server.py
+++ uol_patroller/scripts/engage_server.py
@@ -50,8 +50,6 @@ class EngageServer:
 	def execute(self, goal):
 		self.count = 5
 		# Call Headspin Service 360
-		turn = randint(0,359)	
-		print "Person detected with kinect %i degrees off current position." % turn
 		
 		myBool = False			
 		startTime = time.time()
@@ -99,8 +97,8 @@ class EngageServer:
 			marySpeak.wait_for_result(rospy.Duration.from_sec(0.0))
 
 			rospy.sleep(5)					
-			if time.time() - startTime > goal.mary_seconds:
-				myBool = True
+			#if time.time() - startTime > goal.mary_seconds:
+			myBool = True
 					#monitor person finder and engage for returning finish command 
 					#if round((time.time() - startTime)*1000)
 		
